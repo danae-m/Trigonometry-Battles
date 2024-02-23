9a99030c1c9448cbaa2b3fd87731fba33ac58410
danae-m <131600455+danae-m@users.noreply.github.com>
2024-02-23 12:06:50 -0500

    Update Components.h
    cleaning up artifacts

--- Components.h
+++ Components.h
@@ -99,24 +99,4 @@ public:
 	CInput() {}
 };
 
-/* example component and bounding box class for assgt 3
-* class CTransform : public Component
-* {
-* public:
-*	Vec2 pos = {0.0, 0.0};
-*	Vec2 prevPos = {0.0, 0.0};
-*	Vec2 scale = {1.0, 1.0};
-*	Vec2 speed = {0.0, 0.0};
-*	float angle = 0;
-* };
-* 
-* class CBoundingBox : public Component
-* {
-* public:
-*	Vec2 size;
-*	Vec2 halfSize;
-*	CBoundingBox(const Vec2& s) : size (s), halfSize(s.x / 2, s.y / 2) {}
-* };
-* */
-
 #endif //COMPONENTS_H
